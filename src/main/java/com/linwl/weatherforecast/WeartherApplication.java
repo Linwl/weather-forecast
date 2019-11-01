d4554e39871a853c632de7380e1a260c9e3b1f20
林万龙 <304115325@qq.com>
2019-11-01 17:49:43 +0800

    初始化

---
+++ src/main/java/com/linwl/weatherforecast/WeartherApplication.java
@@ -0,0 +1,17 @@
+package com.linwl.weatherforecast;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author ：linwl
+ * @date ：Created in 2019/11/1 17:45
+ * @description ：
+ * @modified By：
+ */
+@Slf4j
+public class WeartherApplication {
+    public static void main(String[] args)
+    {
+        log.info("启动");
+    }
+}
